0f5f9683463f29aa0a9998a00aa40f7b9c9fff05
DamoM73 <40050142+DamoM73@users.noreply.github.com>
2022-04-04 09:42:39 +1000

    Create game.py

---
+++ game.py
@@ -0,0 +1,14 @@
+class card():
+    
+    def __init__(self,vals):
+        self.name = vals[0]
+        self.intel = vals[1]
+        self.strength = vals[2]
+        self.speed = vals[3]
+        self.durability = vals[4]
+        self.power = vals[5]
+        self.combat = vals[6]
+        self.image = vals[7]
+        self.publisher = vals[8]
+        self.alignment = vals[9]
+        self.alias = vals[10]
